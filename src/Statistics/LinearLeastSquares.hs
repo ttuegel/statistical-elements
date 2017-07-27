119ec24037db7fe001ed70968ac2930858b17cea
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-27 13:42:55 -0500

    LinearLeastSquares: Fit one output variable at a time

--- src/Statistics/LinearLeastSquares.hs
+++ src/Statistics/LinearLeastSquares.hs
@@ -6,28 +6,28 @@ module Statistics.LinearLeastSquares where
 import Linear
 
 
-newtype LeastSquares p k a = LeastSquares (M (p + 1) k a)
+newtype LeastSquares p a = LeastSquares (V (p + 1) a)
 
-fit :: forall a (k :: Nat) (n :: Nat) (p :: Nat).
-       (KnownNat k, KnownNat n, KnownNat p, Semiring a) =>
+fit :: forall a (n :: Nat) (p :: Nat).
+       (KnownNat n, KnownNat p, Semiring a) =>
        M p n a  -- ^ inputs: n samples of a p-vector
-    -> M k n a  -- ^ outputs: n samples of a k-vector
-    -> LeastSquares p k a
+    -> V n a  -- ^ outputs
+    -> LeastSquares p a
 fit inp outp =
   let
     -- lift inp into projective space
     x = projectiveM inp
     -- least squares fit coefficients
-    beta = inv (x ## tr x) ## x ## tr outp
+    beta = inv (x ## tr x) #> (x #> outp)
   in
     LeastSquares beta
 
-predict :: LeastSquares p k a  -- ^ p-by-k fit coefficients
+predict :: LeastSquares p a  -- ^ fit coefficients
         -> V p a  -- ^ p-vector of inputs
-        -> V k a  -- ^ k-vector of predicted outputs
+        -> a  -- ^ predicted output
 predict (LeastSquares beta) inp =
   let
     -- lift inp into projective space
     x = projectiveV inp
   in
-    tr beta #> x
+    beta <.> x
